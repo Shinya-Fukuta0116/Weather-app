c829bb46c710d2f379a350244e0568f3f3ed598c
Shinya-Fukuta0116 <alleyezon.djwanted@gmail.com>
2022-10-27 05:12:58 +0900

    complete

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 window.addEventListener('load', () => {
   let long;
   let lat;
-  let temperatureFeel = document.querySelector('.temperature-feel');
+  let weatherDescriptions = document.querySelector('.weather-descriptions');
   let temperatureDegree = document.querySelector('.temperature-degree');
   let locationTimezone = document.querySelector('.location-timezone');
+  let wind = document.querySelector('.wind');
+  let temperatureSection = document.querySelector('.temperature');
+  const temperatureSpan = document.querySelector('.temperature span');
 
 
   if (navigator.geolocation) {
@@ -19,20 +22,35 @@ window.addEventListener('load', () => {
           return response.json();
         })
         .then(data => {
-          console.log(data);
-          const { temperature, feelslike } = data.current;
+          const { temperature, weather_descriptions } = data.current;
           const { country, region } = data.location;
+          const { wind_dir } = data.current;
           const WEATHER_API_KEY = config.apikey;
           console.log(WEATHER_API_KEY)
           // set DOM Element from the API
           temperatureDegree.textContent = temperature;
-          temperatureFeel.textContent = feelslike;
+          weatherDescriptions.textContent = weather_descriptions;
           locationTimezone.textContent = region, country;
+
+          // Formula for celsius
+          let celsius = (temperature - 32) * (5 / 9);
+
+          wind.textContent = wind_dir;
+
+          // Change temperature to Celsius/Farenheit
+          temperatureSection.addEventListener('click', function () {
+            if (temperatureSpan.textContent == "F") {
+              temperatureSpan.textContent = "C";
+              temperatureDegree.textContent = Math.floor(celsius);
+            } else {
+              temperatureSpan.textContent = "F";
+              temperatureDegree.textContent = temperature;
+            }
+          });
+
+
 
         });
     });
-
-  } else {
-    h1.textContent = "Hey dis is not working because reasons"
   }
 });
